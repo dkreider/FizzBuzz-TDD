147c2c5a4ddea92d7923ee75c3695a5d6493ce1c
Daniel Kreider <daniel@danielk.tech>
2021-08-06 14:21:56 -0300

    Add functionality to make the tests pass

--- FizzBuzz/FizzBuzzer.cs
+++ FizzBuzz/FizzBuzzer.cs
@@ -1,7 +1,21 @@
+using System;
+
 namespace FizzBuzz
 {
     public class FizzBuzzer
     {
-        
+        public static string GetValue(int input)
+        {
+            if (input % 3 == 0 && input % 5 == 0)
+                return "FizzBuzz";
+                
+            if (input % 3 == 0)
+                return "Fizz";
+
+            if (input % 5 == 0)
+                return "Buzz";
+
+            return input.ToString();
+        }
     }
 }
